244e47ebcbf1b2522886d7f2d96494e1b9dbfd0c
taro1115 <496023716@qq.com>
2016-01-10 12:30:11 +0800

    添加处理Activity

--- app/src/main/java/com/qf/githubdemo/MainActivity.java
+++ app/src/main/java/com/qf/githubdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.qf.githubdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        TextView tv = new TextView(this);
+    public void openOther(View view){
+        Intent intent = new Intent();
+        intent.setClass(this,SecondActivity.class);
+        startActivityForResult(intent,100);
     }
 }
