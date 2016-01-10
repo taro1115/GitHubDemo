693b7cd2b9891248c529e77818379666de991bde
taro1115 <496023716@qq.com>
2016-01-10 12:22:36 +0800

    添加新的activity

--- app/src/main/java/com/qf/githubdemo/SecondActivity.java
+++ app/src/main/java/com/qf/githubdemo/SecondActivity.java
@@ -2,15 +2,12 @@ package com.qf.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView tv = new TextView(this);
+        setContentView(R.layout.activity_second);
     }
 }
